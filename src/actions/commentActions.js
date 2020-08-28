3bd2e3626be7e78f7e672f4f375715a5647e7e5b
Karl Wang <kawang@ucsd.edu>
2020-08-28 11:19:18 -0700

    feature: add error handlings for redux actions

--- src/actions/commentActions.js
+++ src/actions/commentActions.js
@@ -8,23 +8,35 @@ import {
 import defaultComments from "../default/Comments.json";
 
 /**
+ * reset the comments in the localStorage in case error occured
+ */
+const resetComments = () => {
+  defaultComments.forEach((comment) => {
+    comment.commentTime = new Date();
+    comment.innerComments.forEach((innerComment) => {
+      innerComment.commentTime = new Date();
+    });
+  });
+  localStorage.setItem("comments", JSON.stringify(defaultComments));
+};
+
+/**
  * fetch the comments from localStorage
  */
 export const getComments = () => (dispatch) => {
-  if (!("comments" in localStorage)) {
-    defaultComments.forEach((comment) => {
-      comment.commentTime = new Date();
-      comment.innerComments.forEach((innerComment) => {
-        innerComment.commentTime = new Date();
-      });
+  try {
+    if (!("comments" in localStorage)) {
+      resetComments();
+    }
+    const comments = JSON.parse(localStorage.getItem("comments"));
+    dispatch({
+      type: GET_COMMENTS,
+      payload: comments,
     });
-    localStorage.setItem("comments", JSON.stringify(defaultComments));
+  } catch (e) {
+    console.error(e);
+    resetComments();
   }
-  const comments = JSON.parse(localStorage.getItem("comments"));
-  dispatch({
-    type: GET_COMMENTS,
-    payload: comments,
-  });
 };
 
 /**
@@ -32,15 +44,20 @@ export const getComments = () => (dispatch) => {
  * @param {Object} comment the new comment to add
  */
 export const addComment = (comment) => (dispatch) => {
-  let comments = [];
-  if ("comments" in localStorage) {
-    comments = JSON.parse(localStorage.getItem("comments"));
+  try {
+    let comments = [];
+    if ("comments" in localStorage) {
+      comments = JSON.parse(localStorage.getItem("comments"));
+    }
+    localStorage.setItem("comments", JSON.stringify([...comments, comment]));
+    dispatch({
+      type: ADD_COMMENT,
+      payload: comment,
+    });
+  } catch (e) {
+    console.error(e);
+    resetComments();
   }
-  localStorage.setItem("comments", JSON.stringify([...comments, comment]));
-  dispatch({
-    type: ADD_COMMENT,
-    payload: comment,
-  });
 };
 
 /**
@@ -49,58 +66,73 @@ export const addComment = (comment) => (dispatch) => {
  * @param {Object} innerComment the new inner comment
  */
 export const addInnerComment = (commentId, innerComment) => (dispatch) => {
-  const comments = JSON.parse(localStorage.getItem("comments"));
-  const outerComment = comments.find((comment) => comment.id === commentId);
-  outerComment.innerComments.push(innerComment);
-  localStorage.setItem("comments", JSON.stringify(comments));
-  dispatch({
-    type: ADD_INNERCOMMENT,
-    payload: {
-      commentId,
-      innerComment,
-    },
-  });
+  try {
+    const comments = JSON.parse(localStorage.getItem("comments"));
+    const outerComment = comments.find((comment) => comment.id === commentId);
+    outerComment.innerComments.push(innerComment);
+    localStorage.setItem("comments", JSON.stringify(comments));
+    dispatch({
+      type: ADD_INNERCOMMENT,
+      payload: {
+        commentId,
+        innerComment,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+    resetComments();
+  }
 };
 
 /**
- * Like the comment with associated comment id
+ * Like/unlike the comment with associated comment id
  * @param {String} commentId the comment id to like
  */
 export const likeComment = (commentId) => (dispatch) => {
-  const comments = JSON.parse(localStorage.getItem("comments"));
-  const comment = comments.find((comment) => comment.id === commentId);
-  comment.commentLiked = !comment.commentLiked;
-  comment.commentLikes += comment.commentLiked ? 1 : -1;
-  localStorage.setItem("comments", JSON.stringify(comments));
-  dispatch({
-    type: LIKE_COMMENT,
-    payload: {
-      commentId,
-      comment,
-    },
-  });
+  try {
+    const comments = JSON.parse(localStorage.getItem("comments"));
+    const comment = comments.find((comment) => comment.id === commentId);
+    comment.commentLiked = !comment.commentLiked;
+    comment.commentLikes += comment.commentLiked ? 1 : -1;
+    localStorage.setItem("comments", JSON.stringify(comments));
+    dispatch({
+      type: LIKE_COMMENT,
+      payload: {
+        commentId,
+        comment,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+    resetComments();
+  }
 };
 
 /**
- * Like a inner comment based with the given outer comment and inner comment id
+ * Like/unlike a inner comment based with the given outer comment and inner comment id
  * @param {String} commentId the outer comment id of the inner comment
  * @param {String} innerCommentId the inner comment id to like
  */
 export const likeInnerComment = (commentId, innerCommentId) => (dispatch) => {
-  const comments = JSON.parse(localStorage.getItem("comments"));
-  const comment = comments.find((comment) => comment.id === commentId);
-  const innerComment = comment.innerComments.find(
-    (innerComment) => innerComment.id === innerCommentId
-  );
-  innerComment.commentLiked = !innerComment.commentLiked;
-  innerComment.commentLikes += innerComment.commentLiked ? 1 : -1;
-  localStorage.setItem("comments", JSON.stringify(comments));
-  dispatch({
-    type: LIKE_INNERCOMMENT,
-    payload: {
-      commentId,
-      innerCommentId,
-      innerComment,
-    },
-  });
+  try {
+    const comments = JSON.parse(localStorage.getItem("comments"));
+    const comment = comments.find((comment) => comment.id === commentId);
+    const innerComment = comment.innerComments.find(
+      (innerComment) => innerComment.id === innerCommentId
+    );
+    innerComment.commentLiked = !innerComment.commentLiked;
+    innerComment.commentLikes += innerComment.commentLiked ? 1 : -1;
+    localStorage.setItem("comments", JSON.stringify(comments));
+    dispatch({
+      type: LIKE_INNERCOMMENT,
+      payload: {
+        commentId,
+        innerCommentId,
+        innerComment,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+    resetComments();
+  }
 };
--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -2,30 +2,46 @@ import { LIKE_POST, GET_POST } from "./types";
 import defaultPost from "../default/Post.json";
 
 /**
+ * reset the post in case error occured
+ */
+const resetPost = () => {
+  defaultPost.postTime = new Date();
+  localStorage.setItem("post", JSON.stringify(defaultPost));
+};
+/**
  * fetch the post information from local storage
  */
 export const getPost = () => (dispatch) => {
-  if (!("post" in localStorage)) {
-    defaultPost.postTime = new Date();
-    localStorage.setItem("post", JSON.stringify(defaultPost));
+  try {
+    if (!("post" in localStorage)) {
+      resetPost();
+    }
+    const post = JSON.parse(localStorage.getItem("post"));
+    dispatch({
+      type: GET_POST,
+      payload: post,
+    });
+  } catch (e) {
+    console.error(e);
+    resetPost();
   }
-  const post = JSON.parse(localStorage.getItem("post"));
-  dispatch({
-    type: GET_POST,
-    payload: post,
-  });
 };
 
 /**
  * handle liking the post
  */
 export const likePost = () => (dispatch) => {
-  const post = JSON.parse(localStorage.getItem("post"));
-  post.postLiked = !post.postLiked;
-  post.postLikes += post.postLiked ? 1 : -1;
-  localStorage.setItem("post", JSON.stringify(post));
-  dispatch({
-    type: LIKE_POST,
-    payload: post,
-  });
+  try {
+    const post = JSON.parse(localStorage.getItem("post"));
+    post.postLiked = !post.postLiked;
+    post.postLikes += post.postLiked ? 1 : -1;
+    localStorage.setItem("post", JSON.stringify(post));
+    dispatch({
+      type: LIKE_POST,
+      payload: post,
+    });
+  } catch (e) {
+    console.error(e);
+    resetPost();
+  }
 };
